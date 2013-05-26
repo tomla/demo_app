e4f60eefe6b10f6033b6077a426c195758bb9230
Tom Lambert <tom.lambert@dataflow.be>
2013-05-26 14:58:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'e3bfb8feb2d6572daf367e7327731a3fa2ea43e3050a090605d080b6905bbdb0f70459c7238a351af753a98b1eaee9eb9490e58ccbce13bc04664a384513c20a'
